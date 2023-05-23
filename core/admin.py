e6c25dbd4f5314cf2c666fc003f3a8eede55c12a
Evgen <admin@seo-rocket.ru>
2023-05-23 13:30:42 +0400

    first commit

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from core.models import *
+from django.contrib import messages
+from django.utils.translation import ngettext
 
-# Register your models here.
+@admin.register(Resident)
+class CategoryAdmin(admin.ModelAdmin):
+    fieldsets = [("Main", {"fields": ['user', 'num']})]
+
+@admin.register(Project)
+class ManagerAdmin(admin.ModelAdmin):
+    fieldsets = [("Main", {"fields": ['name', 'desc', 'cost', 'done']})]
+
+@admin.register(Transactions)
+class ManagerAdmin(admin.ModelAdmin):
+    fieldsets = [("Main", {"fields": ['name','resident','project','cost']})]
