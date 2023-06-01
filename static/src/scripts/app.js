bcdf9e31400badbad1b1552e1f2c4e944ae2d467
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2023-06-01 20:29:48 +0400

    hard_revert

--- static/src/scripts/app.js
+++
@@ -1,9 +0,0 @@
-import {utils} from "./modules/utils.js";
-import {sliders} from "./modules/sliders.js";
-import {forms} from "./modules/forms.js"
-import {animate} from "./modules/animate.js"
-
-utils()
-sliders()
-forms()
-animate()
