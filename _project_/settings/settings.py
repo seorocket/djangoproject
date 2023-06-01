43792a2b65bc1e983adde2abf7fe00b9665cf0a7
Aleksandrov Ivan <101462143+Altorus@users.noreply.github.com>
2023-06-01 20:01:30 +0400

    Revert "upd"
    This reverts commit 03ae3f947fbc1311e182b168900afdde019fbf14.

--- _project_/settings/settings.py
+++ _project_/settings/settings.py
@@ -16,6 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,6 +27,7 @@ SECRET_KEY = 'django-insecure-@3-8%h%mku*90c@_&q%9(j!8fpk5o=r=ya1#@qc!7q_*vp)=&6
 DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
 
 # Application definition
 
@@ -39,7 +41,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-    'rangefilter',
     'corsheaders',
     'ckeditor',
     'ckeditor_uploader',
@@ -61,7 +62,7 @@ ROOT_URLCONF = '_project_.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '..', 'template')],
+        'DIRS': [os.path.join(BASE_DIR, '..',  'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,19 +77,21 @@ TEMPLATES = [
 
 WSGI_APPLICATION = '_project_.wsgi.application'
 
+
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'bullet',
+        'NAME': 'djangoproject',
         'USER': 'pweb',
         'PASSWORD': 'pweb',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -108,10 +111,11 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
@@ -121,17 +125,14 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '..', 'static'),
-)
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '..', 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR,  'media')
 MEDIA_URL = '/media/'
 FILE_UPLOAD_PERMISSIONS = 0o777
 
@@ -139,6 +140,7 @@ FILE_UPLOAD_PERMISSIONS = 0o777
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
 CKEDITOR_ALLOW_NONIMAGE_FILES = False
--- _project_/urls.py
+++ _project_/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
+
+from core.routers import router
 
 urlpatterns = [
-    re_path(r'^', include('core.urls', namespace="core")),
     path('admin/', admin.site.urls),
+    path('api/', include((router.urls, 'core'))),
 ]
